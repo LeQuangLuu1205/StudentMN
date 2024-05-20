5ce18dc09513800443bf3eb1cd78d37696ab1684
Le Quang Luu <lequangluutpqt@gmail.com>
2024-05-20 14:31:44 +0700

    Searching for grade table

---
+++ StudentMG/StudentMG/ViewModels/GradeVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentMG.ViewModels
+{
+    public class GradeVM
+    {
+        [Key]
+        public int No { get; set; }
+        public string Name { get; set; }
+        public int? NoCredits { get; set; }
+        public int? Grade { get; set; }
+
+        public GradeVM() {
+        }
+
+        public GradeVM(int No, string name, int? noCredits, int? grade)
+        {
+            Name = name;
+            NoCredits = noCredits;
+            Grade = grade;
+        }
+    }
+}
---
+++ StudentMG/StudentMG/ViewModels/StudentGradeVM.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentMG.ViewModels
+{
+    public class StudentGradeVM
+    {
+        public string Name { get; set; }
+        public int? NoCredits { get; set; }
+        public int? Grade { get; set; }
+        public StudentGradeVM() { }
+
+        public StudentGradeVM(string name, int? noCredits, int? grade)
+        {
+            Name = name;
+            NoCredits = noCredits;
+            Grade = grade;
+        }
+    }
+}
